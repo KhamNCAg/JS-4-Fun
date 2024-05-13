e05f377975e15192893683004aa509e67017e351
KhamNCAg <kham.nyein.chan.aung@gmail.com>
2024-05-13 16:37:45 +0700

    163713MAY

--- Unicode Generator/script.js
+++ Unicode Generator/script.js
@@ -1,33 +1,11 @@
-let dateContainer = document.querySelector(".date-container");
-let hoursContainer = document.querySelector(".hours");
-let minsContainer = document.querySelector(".minutes");
-let secsContainer = document.querySelector(".seconds");
+function getChar(){
 
-const weekdays = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    const input = document.getElementById("inptChar").value;
 
-const months = ["January","February","March","April","May","June","July","August","September","October","November","December"]
-
-setInterval(updateClock,1000);
-
-function updateClock(){
-    const today = new Date();
-
-    let hours = formatTime(today.getHours());
-    let minutes = formatTime(today.getMinutes());
-    let seconds = formatTime(today.getSeconds());
-
-    let day = weekdays[today.getDay()];
-    let date = today.getDate();
-    let month = months[today.getMonth()];
-
-    hoursContainer.textContent = hours+":";
-    minsContainer.textContent = minutes+":";
-    secsContainer.textContent = seconds;
-
-    dateContainer.innerHTML = `<p>${day}</p><span>${date}</span><p>${month}</p>`
-
-}
-
-function formatTime(time){
-    return time < 10 ? "0"+time : time;
+    if(input){
+        const uniVlu = input.charCodeAt(0);
+        document.getElementById("result").textContent = `The Unicode Value of "${input}" is ${uniVlu}`
+    }else{
+        document.getElementById("result").textContent = 'Please Enter a Character.'
+    }
 }
