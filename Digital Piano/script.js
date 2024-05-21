b406a3edcfb646b0ff9ad7dce1e994baba01b7a6
KhamNCAg <kham.nyein.chan.aung@gmail.com>
2024-05-21 22:49:57 +0700

    224921MAY

--- Digital Piano/script.js
+++ Digital Piano/script.js
@@ -1,18 +1,18 @@
 var keyName;
 const tmpObj = [];
-function startSound(frequency,keyIndx){
-  // console.log(keyIndx + "start");
+function startSound(frequency,keyIndx,typ){
   var tmpCont = new AudioContext();
   var tmpOsci = tmpCont.createOscillator();
   var tmpGain = tmpCont.createGain();
 
-  console.log(tmpCont.state);
-
   tmpOsci.connect(tmpGain);
   tmpGain.connect(tmpCont.destination);
   tmpOsci.frequency.value = frequency;
-  // tmpOsci.type = "triangle";
-  tmpOsci.type = "sine";
+  if (typ=="tri"){
+    tmpOsci.type = "triangle";
+  }else if(typ=="sin"){
+    tmpOsci.type = "sine";
+  }
   tmpOsci.start(0);
   tmpObj[keyIndx] = new Array(tmpCont, tmpOsci, tmpGain, true);
 }
@@ -26,37 +26,63 @@ function stopSound(keyIndx){
 
 document.addEventListener('keydown', (event) => {
   keyName = event.key;
+  keyName = (keyName.charCodeAt(0));
+  console.log(keyName);
   if(!(tmpObj[keyName] && tmpObj[keyName][3])){
-    if(keyName==1){
-      startSound(1047,keyName);
-      // startSound(130.8,keyName);
-    }else if(keyName==2){
-      startSound(1175,keyName);
-      // startSound(146.8,keyName);
-    }else if(keyName==3){
-      startSound(1319,keyName);
-      // startSound(164.8,keyName);
-    }else if(keyName==4){
-      startSound(1397,keyName);
-      // startSound(174.6,keyName);
-    }else if(keyName==5){
-      startSound(1568,keyName);
-      // startSound(196.0,keyName);
-    }else if(keyName==6){
-      startSound(1760,keyName);
-      // startSound(220.0,keyName);
-    }else if(keyName==7){
-      startSound(1976,keyName);
-      // startSound(246.9,keyName);
-    }else if(keyName==8){
-      startSound(2093,keyName);
-      // startSound(261.6,keyName);
+    if(keyName==113){
+      startSound(130.8,keyName,"tri");
+    }else if(keyName==119){
+      startSound(146.8,keyName,"tri");
+    }else if(keyName==101){
+      startSound(164.8,keyName,"tri");
+    }else if(keyName==114){
+      startSound(174.6,keyName,"tri");
+    }else if(keyName==116){
+      startSound(196.0,keyName,"tri");
+    }else if(keyName==121){
+      startSound(220.0,keyName,"tri");
+    }else if(keyName==117){
+      startSound(246.9,keyName,"tri");
+    }else if(keyName==105){
+      startSound(261.6,keyName,"tri");
+    }
+    else if(keyName==97){
+      startSound(523.3,keyName,"sin");
+    }else if(keyName==115){
+      startSound(587.3,keyName,"sin");
+    }else if(keyName==100){
+      startSound(659.3,keyName,"sin");
+    }else if(keyName==102){
+      startSound(698.5,keyName,"sin");
+    }else if(keyName==103){
+      startSound(784.0,keyName,"sin");
+    }else if(keyName==104){
+      startSound(880.0,keyName,"sin");
+    }else if(keyName==106){
+      startSound(987.8,keyName,"sin");
+    }else if(keyName==122){
+      startSound(1047,keyName,"sin");
+    }else if(keyName==120){
+      startSound(1175,keyName,"sin");
+    }else if(keyName==99){
+      startSound(1319,keyName,"sin");
+    }else if(keyName==118){
+      startSound(1397,keyName,"sin");
+    }else if(keyName==98){
+      startSound(1568,keyName,"sin");
+    }else if(keyName==110){
+      startSound(1760,keyName,"sin");
+    }else if(keyName==109){
+      startSound(1976,keyName,"sin");
+    }else if(keyName==44){
+      startSound(2093,keyName,"sin");
     }
   }
 }, false);
 
 document.addEventListener('keyup', (event) => {
   keyName = event.key;
+  keyName = (keyName.charCodeAt(0));
   if(tmpObj[keyName] && tmpObj[keyName][3]){
     stopSound(keyName);
   }  
