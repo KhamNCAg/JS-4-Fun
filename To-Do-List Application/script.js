a3c36bad721da070474aa9821d28224bfbe404b2
KhamNCAg <kham.nyein.chan.aung@gmail.com>
2024-05-23 07:20:27 +0700

    072023MAY

--- To-Do-List Application/script.js
+++ To-Do-List Application/script.js
@@ -1,60 +1,49 @@
-var keyName, preKey;
-const tmpObj = [];
-function startSound(frequency,keyIndx){
-  // console.log(keyIndx + "start");
-  var tmpCont = new AudioContext()
-  var tmpOsci = tmpCont.createOscillator()
-  var tmpGain = tmpCont.createGain()
-  tmpOsci.connect(tmpGain)
-  tmpGain.connect(tmpCont.destination)
-  tmpOsci.frequency.value = frequency
-  // tmpOsci.type = "triangle"
-  tmpOsci.type = "sine"
-  tmpOsci.start(0)
-  tmpObj[keyIndx] = new Array(tmpCont, tmpOsci, tmpGain, true);
-}
+const listContainer = document.getElementById("list-container");
+const inputBox = document.getElementById("input-box");
 
-function stopSound(keyIndx){
-  // console.log(keyIndx + "stop");
-  tmpObj[keyIndx][2].gain.exponentialRampToValueAtTime(0.00001, tmpObj[keyIndx][0].currentTime + 1.5 )
-  tmpObj[keyIndx][1].stop(tmpObj[keyIndx][0].currentTime + 1.5)
-  tmpObj[keyIndx][3] = 0;
-}
 
-document.addEventListener('keydown', (event) => {
-  keyName = event.key;
-  if(!(tmpObj[keyName] && tmpObj[keyName][3])){
-    if(keyName==1){
-      startSound(1047,keyName);
-      // startSound(130.8,keyName);
-    }else if(keyName==2){
-      startSound(1175,keyName);
-      // startSound(146.8,keyName);
-    }else if(keyName==3){
-      startSound(1319,keyName);
-      // startSound(164.8,keyName);
-    }else if(keyName==4){
-      startSound(1397,keyName);
-      // startSound(174.6,keyName);
-    }else if(keyName==5){
-      startSound(1568,keyName);
-      // startSound(196.0,keyName);
-    }else if(keyName==6){
-      startSound(1760,keyName);
-      // startSound(220.0,keyName);
-    }else if(keyName==7){
-      startSound(1976,keyName);
-      // startSound(246.9,keyName);
-    }else if(keyName==8){
-      startSound(2093,keyName);
-      // startSound(261.6,keyName);
+function addTask(){
+
+    if(inputBox.value === ''){
+        alert("Enter Some Data");
+    }else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+
     }
-  }
-}, false);
 
-document.addEventListener('keyup', (event) => {
-  keyName = event.key;
-  if(tmpObj[keyName] && tmpObj[keyName][3]){
-    stopSound(keyName);
-  }  
-}, false);
+    inputBox.value = "";
+    saveTask();
+
+
+}
+
+listContainer.addEventListener("click",function(e){
+
+
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveTask();
+    }else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveTask();
+    }
+
+});
+
+
+function saveTask(){
+    localStorage.setItem("data",listContainer.innerHTML);
+}
+
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+
+
+showTask();
